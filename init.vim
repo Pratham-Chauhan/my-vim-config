9742b2cac5c0d066f0104aba9f2e725ee28ebd70
Pratham Chauhan (PC) <prathamchauhan.coding42@gmail.com>
2023-08-02 00:05:10 +0530

    vim config

---
+++ init.vim
@@ -0,0 +1,75 @@
+call plug#begin('~/.local/share/nvim/plugged')
+" Themes
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'morhetz/gruvbox'
+Plug 'folke/tokyonight.nvim'
+Plug 'marko-cerovac/material.nvim'
+
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+
+" nvim-cmp autocompletion
+Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+
+Plug 'hrsh7th/cmp-vsnip'
+Plug 'hrsh7th/vim-vsnip'
+" fzf
+Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+Plug 'junegunn/fzf.vim'
+
+" nvim-ufo
+Plug 'kevinhwang91/promise-async'
+Plug 'kevinhwang91/nvim-ufo'
+
+Plug 'numToStr/FTerm.nvim'
+Plug 'onsails/lspkind.nvim'
+Plug 'lukas-reineke/indent-blankline.nvim'
+
+Plug 'dccsillag/magma-nvim', { 'do': ':UpdateRemotePlugins' }
+Plug 'sbdchd/neoformat'
+Plug 'Pocco81/auto-save.nvim'
+call plug#end()
+
+set number
+set relativenumber
+set nowrap
+set clipboard+=unnamedplus
+
+set guifont=Hack:h14
+
+" Magma Jupyter Notebook
+" nnoremap <silent><expr> <LocalLeader>r  :MagmaEvaluateOperator<CR>
+nnoremap <silent>       <LocalLeader>rr :MagmaEvaluateLine<CR>
+xnoremap <silent>       <LocalLeader>r  :<C-u>MagmaEvaluateVisual<CR>
+nnoremap <silent>       <LocalLeader>rc :MagmaReevaluateCell<CR>
+nnoremap <silent>       <LocalLeader>rd :MagmaDelete<CR>
+nnoremap <silent>       <LocalLeader>ro :MagmaShowOutput<CR>
+
+let g:magma_automatically_open_output = v:false
+" let g:magma_image_provider = "ueberzug"
+
+nnoremap pp :e $MYVIMRC<CR>
+nnoremap _u :w \|source $MYVIMRC<CR>
+nnoremap <silent> <C-B> :Buffers<CR>
+nnoremap <silent> <C-F> :Files<CR>
+nnoremap <silent> <C-H> :History<CR>
+
+autocmd FileType python map <buffer> <F5> :w<CR>:!python3 %<CR>
+autocmd FileType python imap <buffer> <F5> <esc>:w<CR>:!python3 %<CR>
+set background=dark    " Setting dark mode
+
+let g:gruvbox_italic = 1
+let g:gruvbox_underline = 1
+let g:gruvbox_contrast_dark = 'medium'
+let g:gruvbox_number_column = 'bg0'
+
+let g:airline_theme='durant'
+
+colorscheme tokyonight-night
+
+lua require'init'
---
+++ lua/init.lua
@@ -0,0 +1,158 @@
+require("nvim-treesitter.install").prefer_git = true
+require "nvim-treesitter.configs".setup {
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false
+    },
+    prefer_git = true
+}
+require("tokyonight").setup({transparent = false})
+-- Set up nvim-cmp.
+local cmp = require "cmp"
+
+local lspkind = require("lspkind")
+cmp.setup(
+    {
+        snippet = {
+            -- REQUIRED - you must specify a snippet engine
+            expand = function(args)
+                vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+                -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            end
+        },
+        mapping = cmp.mapping.preset.insert(
+            {
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<Tab>"] = cmp.mapping.confirm({select = true}) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }
+        ),
+        sources = cmp.config.sources(
+            {
+                {name = "nvim_lsp"},
+                {name = "vsnip"} -- For vsnip users.
+            },
+            {
+                {name = "buffer"}
+            }
+        ),
+        formatting = {
+            format = lspkind.cmp_format(
+                {
+                    mode = "symbol", -- show only symbol annotations
+                    maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                    ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                    before = function(entry, vim_item)
+                        return vim_item
+                    end
+                }
+            )
+        }
+    }
+)
+
+-- LSP Config
+local lspconfig = require "lspconfig"
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+lspconfig.jedi_language_server.setup {capabilities = capabilities}
+
+
+
+
+vim.opt.list = true
+-- vim.opt.listchars:append "space:⋅"
+
+require("indent_blankline").setup {
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+    show_end_of_line = false,
+}
+
+require("auto-save").setup{}
+
+
+vim.o.foldcolumn = "1" -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
+-- Option 3: treesitter as a main provider instead
+-- Only depend on `nvim-treesitter/queries/filetype/folds.scm`,
+-- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
+require("ufo").setup(
+    {
+        provider_selector = function(bufnr, filetype, buftype)
+            return {"treesitter", "indent"}
+        end
+    }
+)
+
+-- FTerm.nvim
+vim.api.nvim_create_user_command("FTermOpen", require("FTerm").open, {bang = true})
+vim.api.nvim_create_user_command("FTermClose", require("FTerm").close, {bang = true})
+vim.api.nvim_create_user_command("FTermToggle", require("FTerm").toggle, {bang = true})
+
+-- vim.api.nvim_create_user_command('FormatJSON', 'w')
+vim.keymap.set({"n", "t"}, "<C-j>", "<cmd>FTermToggle<CR>")
+vim.keymap.set({"n", "v"}, "<C-s>", "<cmd>w<CR>")
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd(
+    "LspAttach",
+    {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+            -- Enable completion triggered by <c-x><c-o>
+            vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+            -- Buffer local mappings.
+            -- See `:help vim.lsp.*` for documentation on any of the below functions
+            local opts = {buffer = ev.buf}
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+            vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+            vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+            vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+            vim.keymap.set(
+                "n",
+                "<space>wl",
+                function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end,
+                opts
+            )
+            vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+            vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+            vim.keymap.set({"n", "v"}, "<space>ca", vim.lsp.buf.code_action, opts)
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+            vim.keymap.set(
+                "n",
+                "<space>f",
+                function()
+                    vim.lsp.buf.format {async = true}
+                end,
+                opts
+            )
+        end
+    }
+)
